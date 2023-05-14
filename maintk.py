78957241ba37538cc62fc1a0b704dde3a9bf5ec8
Endriw <endriwbatistela@gmail.com>
2023-05-13 23:17:52 -0300

    Added bootstrap (darkly theme)

--- maintk.py
+++ maintk.py
@@ -1,10 +1,39 @@
 from tkinter import *
-from tkinter import ttk
+'''from tkinter import ttk'''
+import ttkbootstrap as ttk
 
 
-root = Tk()
+miles = ''
+def convert():
+    kms = float(entry_int.get())
+    miles = kms * 1.61
+    output_string.set(miles)
+
+# window root
+root = ttk.Window(themename= 'darkly')
+root.title('Tutorial')
+root.geometry('800x600')
 frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text='Sua mãe', font='helvetica').grid(column=0, row=0)
-ttk.Button(frm, text='sair', command=root.destroy).grid(column=0, row=1)
+
+# title
+title_label = ttk.Label(master=root,  text='Kilometers to Miles', font='helvetica 24')
+title_label.pack(pady =10)
+
+#input
+input_frame = ttk.Frame(master=root)
+entry_int = IntVar()
+entry = ttk.Entry(master = input_frame, textvariable=entry_int)
+butt1 = ttk.Button(master = input_frame, text='Convert', command = convert)
+entry.pack(side = 'left', padx = 10)
+butt1.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+# output
+output_string = StringVar()
+output_label = ttk.Label(master=root,
+             text = f'{miles} miles',
+             font ='Helvetica 24',
+             textvariable = output_string)
+output_label.pack(pady=5)
+
 root.mainloop()
