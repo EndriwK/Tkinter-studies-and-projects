6de6f14caeb5d490cccdf8f7b8f4023c27c23145
Endriw <endriwbatistela@gmail.com>
2023-05-14 13:21:15 -0300

    added enable/disable feature

--- ConfigMethod.py
+++ ConfigMethod.py
@@ -10,7 +10,14 @@ main_window.title('Label update')
 def update_function():
     print(entry.get())
     label['text'] = entry.get()
+    entry['state'] = 'disabled'
 
+# enable entry button
+def enable_function():
+    label['text'] = 'Update it!'
+    entry['state'] = 'enabled'
+    
+    
 # label
 label= ttk.Label(master=main_window, text='Update it!')
 label.pack(pady=10)
@@ -19,8 +26,11 @@ label.pack(pady=10)
 entry = ttk.Entry(master=main_window)
 entry.pack(pady=10)
 
-# button
+# button 1
 button = ttk.Button(master=main_window, text='Update', command=update_function)
 button.pack(pady=10)
 
+# button 2
+enable_button = ttk.Button(master=main_window, text='Enable', command=enable_function)
+enable_button.pack()
 main_window.mainloop()
