cc6015b01ea218b5b3c05840fae30b630ac47dd3
Endriw <endriwbatistela@gmail.com>
2023-05-14 00:39:57 -0300

    added simple text editor project

---
+++ SimpleTextEditor.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+
+# main widnow parameters
+main_window = ttk.Window(themename='cyborg')
+main_window.geometry('800x500')
+main_window.title('Text editor')
+
+# save file function
+def savefile():
+    edited_txt = text_editor.get('1.0', 'end')
+    print(f'final text: {edited_txt}')
+    file_name = entry_string.get()
+    print(f'file name: {file_name}')
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(edited_txt)
+
+# text editor
+text_editor = ttk.Text(master=main_window, height=8)
+text_editor.pack()
+
+# file name entry
+entry_string = tk.StringVar()
+entry_name = ttk.Entry(master=main_window, textvariable=entry_string)
+entry_name.pack(side='left',padx=100, pady=5)
+
+# save file button
+save_button = ttk.Button(master=main_window, text='save file', command=savefile)
+save_button.pack(side='right',padx=100, pady=5)
+
+# main loop
+main_window.mainloop()
+
+# .get() method for text accepts 2 arguments get('1.0', 'end')
+# for example will get a string from line(1).character(0) til the end
+# of the textbox
+# open('filename.txt', 'mode (w, a, + )') as x:
+#   x.write(content)
