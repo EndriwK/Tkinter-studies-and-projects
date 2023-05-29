4d3e1dcd911e0da9afcba489ea360c7f0921f145
Endriw <endriwbatistela@gmail.com>
2023-05-29 13:12:51 -0300

    added final mp3 download feature

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -19,9 +19,13 @@ link_entry.pack(pady=10)
 def mp3_download():
     link = link_entry.get()
     yt = YouTube(link)
-    stream = yt.streams.filter(only_audio=True).first()
-    video = 
-    stream.download()
+    video = yt.streams.filter(only_audio=True).first()
+    destination = '.'
+    out_file = video.download(output_path=destination)
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
     print('Downloaded MP3')
     
 # button
